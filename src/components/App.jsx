38a127e648d71815226fe1afc1c9151fb5f39888
Serhii Kravchenko <kravchenko087@ukr.net>
2022-08-29 13:40:00 +0300

    fix сброс страницы при новом имени

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,11 +22,11 @@ export class App extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.name !== this.state.name) {
-      this.setState({ status: 'pending', arrayImage: [] })
+      this.setState({ status: 'pending', arrayImage: [], page: 1 })
     }
     if (prevState.name !== this.state.name || prevState.page !== this.state.page) {
       this.setState({ status: 'pending', })
-      fetch(`https://pixabay.com/api/?q=${this.state.name}&page=1&key=${KEY}&image_type=photo&orientation=horizontal&page=${this.state.page}&per_page=12`)
+      fetch(`https://pixabay.com/api/?q=${this.state.name}&key=${KEY}&image_type=photo&orientation=horizontal&page=${this.state.page}&per_page=12`)
         .then(res => res.json())
         .then(({ hits }) => {
           if (hits.length === 0) {
